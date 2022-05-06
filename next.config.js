bdd5f1833cdfb62e4fe05080c30740de8f175b8d
Madandola Hammed <aramicrown@gmail.com>
2022-05-06 09:18:42 +0100

    add stable pool routes

--- next.config.js
+++ next.config.js
@@ -1,109 +1,121 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require( 'next-pwa' )
+const runtimeCaching = require( 'next-pwa/cache' )
 
-const linguiConfig = require('./lingui.config.js')
+const linguiConfig = require( './lingui.config.js' )
 
 const { locales, sourceLocale } = linguiConfig
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const withBundleAnalyzer = require( '@next/bundle-analyzer' )( {
+    enabled: process.env.ANALYZE === 'true',
+} )
 
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules = [
-      ...config.module.rules,
-      {
-        resourceQuery: /raw-lingui/,
-        type: 'javascript/auto',
-      },
-    ]
+    webpack: ( config ) => {
+        config.module.rules = [
+            ...config.module.rules,
+            {
+                resourceQuery: /raw-lingui/,
+                type: 'javascript/auto',
+            },
+        ]
 
-    return config
-  },
-  experimental: { esmExternals: true },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: process.env.NODE_ENV === 'development',
-  },
-  images: {
-    domains: ['assets.sushi.com', 'res.cloudinary.com', 'raw.githubusercontent.com', 'logos.covalenthq.com'],
-  },
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/swap',
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/yield',
-        destination: '/farm',
-      },
-      {
-        source: '/stake',
-        destination: '/stake',
-      },
-      {
-        source: '/boost',
-        destination: '/boost',
-      },
-      {
-        source: '/add/:token*',
-        destination: '/exchange/add/:token*',
-      },
-      {
-        source: '/remove/:token*',
-        destination: '/exchange/remove/:token*',
-      },
-      {
-        source: '/create/:token*',
-        destination: '/exchange/add/:token*',
-      },
-      {
-        source: '/swap',
-        destination: '/exchange/swap',
-      },
-      {
-        source: '/swap/:token*',
-        destination: '/exchange/swap/:token*',
-      },
-      {
-        source: '/migrate',
-        destination: '/exchange/migrate',
-      },
-      {
-        source: '/pool',
-        destination: '/exchange/pool',
-      },
-      {
-        source: '/find',
-        destination: '/exchange/find',
-      },
-      {
-        source: '/prisale',
-        destination: '/prisale',
-      },
-      {
-        source: '/migrate',
-        destination: '/migrate',
-      },
-    ]
-  },
-  i18n: {
-    localeDetection: true,
-    locales,
-    defaultLocale: sourceLocale,
-  },
+        return config
+    },
+    experimental: { esmExternals: true },
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+        disable: process.env.NODE_ENV === 'development',
+    },
+    images: {
+        domains: ['assets.sushi.com', 'res.cloudinary.com', 'raw.githubusercontent.com', 'logos.covalenthq.com'],
+    },
+    reactStrictMode: true,
+    async redirects () {
+        return [
+            {
+                source: '/',
+                destination: '/swap',
+                permanent: true,
+            },
+        ]
+    },
+    async rewrites () {
+        return [
+            {
+                source: '/yield',
+                destination: '/farm',
+            },
+            {
+                source: '/stake',
+                destination: '/stake',
+            },
+            {
+                source: '/boost',
+                destination: '/boost',
+            },
+            {
+                source: '/add/:token*',
+                destination: '/exchange/add/:token*',
+            },
+            {
+                source: '/remove/:token*',
+                destination: '/exchange/remove/:token*',
+            },
+            {
+                source: '/create/:token*',
+                destination: '/exchange/add/:token*',
+            },
+            {
+                source: '/swap',
+                destination: '/exchange/swap',
+            },
+            {
+                source: '/swap/:token*',
+                destination: '/exchange/swap/:token*',
+            },
+            {
+                source: '/migrate',
+                destination: '/exchange/migrate',
+            },
+            {
+                source: '/pool',
+                destination: '/exchange/pool',
+            },
+            {
+                source: '/find',
+                destination: '/exchange/find',
+            },
+            {
+                source: '/prisale',
+                destination: '/prisale',
+            },
+            {
+                source: '/migrate',
+                destination: '/migrate',
+            },
+            {
+                source: '/stable-pool',
+                destination: '/exchange-stable/pool',
+            },
+            {
+                source: '/stable-pool/add/:pool*',
+                destination: '/exchange-stable/add/:pool*',
+            },
+            {
+                source: '/stable-pool/remove/:pool*',
+                destination: '/exchange-stable/remove/:pool*',
+            },
+        ]
+    },
+    i18n: {
+        localeDetection: true,
+        locales,
+        defaultLocale: sourceLocale,
+    },
 }
 
-module.exports = withBundleAnalyzer(withPWA(nextConfig))
+module.exports = withBundleAnalyzer( withPWA( nextConfig ) )
 
 // Don't delete this console log, useful to see the config in Vercel deployments
-console.log('next.config.js', JSON.stringify(module.exports, null, 2))
+console.log( 'next.config.js', JSON.stringify( module.exports, null, 2 ) )
