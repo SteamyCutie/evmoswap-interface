9c692ed54a17d9383d465c782e5c50baa56dbd3a
Madandola Hammed <aramicrown@gmail.com>
2022-05-13 21:48:18 +0100

    fix: parse amount only when token is defined

--- src/features/farm/hooks.ts
+++ src/features/farm/hooks.ts
@@ -8,86 +8,86 @@ import { NEVER_RELOAD, useSingleCallResult } from 'app/state/multicall/hooks'
 import { useToken } from 'app/hooks/Tokens'
 
 // @ts-ignore TYPE NEEDS FIXING
-export function useUserInfo(farm, token) {
-  const { account } = useActiveWeb3React()
+export function useUserInfo ( farm, token ) {
+    const { account } = useActiveWeb3React()
 
-  const contract = useMasterChefContract()
+    const contract = useMasterChefContract()
 
-  const args = useMemo(() => {
-    if (!account) {
-      return
+    const args = useMemo( () => {
+        if ( !account ) {
+            return
+        }
+        return [ String( farm.pid ), String( account ) ]
+    }, [ farm, account ] )
+
+    const result = useSingleCallResult( args ? contract : null, 'userInfo', args )?.result
+
+    const value = result?.[ 0 ]
+
+    const amount = value ? JSBI.BigInt( value.toString() ) : undefined
+
+    return {
+        stakedAmount: amount && token ? CurrencyAmount.fromRawAmount( token, amount ) : undefined,
     }
-    return [String(farm.pid), String(account)]
-  }, [farm, account])
-
-  const result = useSingleCallResult(args ? contract : null, 'userInfo', args)?.result
-
-  const value = result?.[0]
-
-  const amount = value ? JSBI.BigInt(value.toString()) : undefined
-
-  return {
-    stakedAmount: amount ? CurrencyAmount.fromRawAmount(token, amount) : undefined,
-  }
 }
 
-export function usePendingReward(farm) {
-  const { account } = useActiveWeb3React()
-  const contract = useMasterChefContract()
-  const args = [String(farm.pid), String(account)]
-  const [reward, setReward] = useState<any>()
-  const getReward = async () => {
-    setReward(await contract.pendingTokens(...args))
-  }
-  useEffect(() => {
-    getReward()
-  }, [])
+export function usePendingReward ( farm ) {
+    const { account } = useActiveWeb3React()
+    const contract = useMasterChefContract()
+    const args = [ String( farm.pid ), String( account ) ]
+    const [ reward, setReward ] = useState<any>()
+    const getReward = async () => {
+        setReward( await contract.pendingTokens( ...args ) )
+    }
+    useEffect( () => {
+        getReward()
+    }, [] )
 
-  return reward
+    return reward
 }
 
 export const useEmoUsdcPrice = (): BigNumber | undefined => {
-  const dashboard = useDashboardContract()
-  return useSingleCallResult(dashboard, 'rewardPriceInUSD')?.result?.[0]
+    const dashboard = useDashboardContract()
+    return useSingleCallResult( dashboard, 'rewardPriceInUSD' )?.result?.[ 0 ]
 }
 
-export function useTokenInfo(tokenContract?: Contract | null) {
-  const _totalSupply = useSingleCallResult(tokenContract ? tokenContract : null, 'totalSupply', undefined, NEVER_RELOAD)
-    ?.result?.[0]
+export function useTokenInfo ( tokenContract?: Contract | null ) {
+    const _totalSupply = useSingleCallResult( tokenContract ? tokenContract : null, 'totalSupply', undefined, NEVER_RELOAD )
+        ?.result?.[ 0 ]
 
-  const _burnt = useSingleCallResult(
-    tokenContract ? tokenContract : null,
-    'balanceOf',
-    ['0x000000000000000000000000000000000000dEaD'],
-    NEVER_RELOAD
-  )?.result?.[0]
+    const _burnt = useSingleCallResult(
+        tokenContract ? tokenContract : null,
+        'balanceOf',
+        [ '0x000000000000000000000000000000000000dEaD' ],
+        NEVER_RELOAD
+    )?.result?.[ 0 ]
 
-  const totalSupply = _totalSupply ? JSBI.BigInt(_totalSupply.toString()) : JSBI.BigInt(0)
-  const burnt = _burnt ? JSBI.BigInt(_burnt.toString()) : JSBI.BigInt(0)
+    const totalSupply = _totalSupply ? JSBI.BigInt( _totalSupply.toString() ) : JSBI.BigInt( 0 )
+    const burnt = _burnt ? JSBI.BigInt( _burnt.toString() ) : JSBI.BigInt( 0 )
 
-  const circulatingSupply = JSBI.subtract(totalSupply, burnt)
+    const circulatingSupply = JSBI.subtract( totalSupply, burnt )
 
-  const token = useToken(tokenContract.address)
+    const token = useToken( tokenContract.address )
 
-  return useMemo(() => {
-    if (!token) {
-      return {
-        totalSupply: '0',
-        burnt: '0',
-        circulatingSupply: '0',
-      }
-    }
+    return useMemo( () => {
+        if ( !token ) {
+            return {
+                totalSupply: '0',
+                burnt: '0',
+                circulatingSupply: '0',
+            }
+        }
 
-    return {
-      totalSupply: CurrencyAmount.fromRawAmount(token, totalSupply).toFixed(0),
-      burnt: CurrencyAmount.fromRawAmount(token, burnt).toFixed(0),
-      circulatingSupply: CurrencyAmount.fromRawAmount(token, circulatingSupply).toFixed(0),
-    }
-  }, [totalSupply, burnt, circulatingSupply, token])
+        return {
+            totalSupply: CurrencyAmount.fromRawAmount( token, totalSupply ).toFixed( 0 ),
+            burnt: CurrencyAmount.fromRawAmount( token, burnt ).toFixed( 0 ),
+            circulatingSupply: CurrencyAmount.fromRawAmount( token, circulatingSupply ).toFixed( 0 ),
+        }
+    }, [ totalSupply, burnt, circulatingSupply, token ] )
 }
 
-export function useInfiniteScroll(items): [number, Dispatch<number>] {
-  const [itemsDisplayed, setItemsDisplayed] = useState(10)
-  useEffect(() => setItemsDisplayed(10), [items.length])
-  return [itemsDisplayed, setItemsDisplayed]
+export function useInfiniteScroll ( items ): [ number, Dispatch<number> ] {
+    const [ itemsDisplayed, setItemsDisplayed ] = useState( 10 )
+    useEffect( () => setItemsDisplayed( 10 ), [ items.length ] )
+    return [ itemsDisplayed, setItemsDisplayed ]
 }
