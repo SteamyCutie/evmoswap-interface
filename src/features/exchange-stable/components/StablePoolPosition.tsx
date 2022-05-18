1cca2032ba8c43f321a3815627f01f0c54606022
Madandola Hammed <aramicrown@gmail.com>
2022-05-18 12:52:02 +0100

    feat: add stable pool position info

--- src/features/exchange-stable/components/StablePoolPosition.tsx
+++ src/features/exchange-stable/components/StablePoolPosition.tsx
@@ -1,29 +1,42 @@
+import { CurrencyAmount, Token } from '@evmoswap/core-sdk'
 import { i18n } from '@lingui/core'
 import { t } from '@lingui/macro'
 import Dots from 'app/components/Dots'
 import Empty from 'app/components/Empty'
+import QuestionHelper from 'app/components/QuestionHelper'
 import { RowBetween } from 'app/components/Row'
-import { STABLE_POOLS } from 'app/constants/pools'
-import { classNames, formatPercent } from 'app/functions'
+import { StablePoolInfo } from 'app/constants/pools'
+import { classNames, formatNumber, formatNumberPercentage } from 'app/functions'
+import { useTotalSupply } from 'app/hooks/useTotalSupply'
 import { useActiveWeb3React } from 'app/services/web3'
-import { useStablePoolInfo } from '../hooks'
+import { useTokenBalance } from 'app/state/wallet/hooks'
+import { useMemo } from 'react'
+import { useStableTokenToReceive } from '../hooks'
 
-const StablePoolInfo = ( {
-    poolId,
-    className = '',
-}: {
-    poolId: string
-    showHeader?: boolean
-    className?: string
-} ) => {
-    const { chainId } = useActiveWeb3React()
-    const pool = STABLE_POOLS[ chainId ][ poolId ]
+const StablePoolPosition = ( { poolInfo, className = '' }: { poolInfo: StablePoolInfo, className?: string } ) => {
 
-    const poolInfo = useStablePoolInfo( poolId )
-    const poolTokensInfo = poolInfo.tokensInfo
+    const { account, chainId } = useActiveWeb3React()
+    const poolId = poolInfo?.pid;
+
     const isLoading = poolInfo?.isLoading
-    const totalTvl = Number( poolTokensInfo?.total ) * Number( poolInfo?.virtualPrice )
-    const poolTokenPercentage = totalTvl
+    const tokens = poolInfo?.pooledTokensInfo?.tokens
+    const lpToken = poolInfo?.lpToken
+
+    const balance = useTokenBalance( account, lpToken ? new Token( chainId, lpToken.address, lpToken.decimals, lpToken.symbol ) : undefined );
+    const totalLpSupply = useTotalSupply( balance?.currency )
+
+    const estimatedSLPs = useStableTokenToReceive( poolId, balance );
+    const liquidityTokensValue = useMemo( () => {
+
+        if ( !estimatedSLPs || !tokens ) return;
+
+        return estimatedSLPs.map( ( estimate, index ) => {
+            return CurrencyAmount.fromRawAmount( tokens[ index ], estimate ?? "0" )
+        } )
+
+    }, [ estimatedSLPs, tokens ] )
+
+
     return (
         <>
             { isLoading && (
@@ -32,16 +45,32 @@ const StablePoolInfo = ( {
                 </Empty>
             ) }
 
-            { pool && (
+            { !isLoading && (
                 <div className={ classNames( 'px-4 py-4 space-y-1 text-sm rounded text-high-emphesis bg-dark-900', className ) }>
                     <RowBetween>
-                        <div>{ i18n._( t`Your TVL` ) }:</div>
-                        <div className="font-semibold">{ totalTvl ? totalTvl : '-' }</div>
+                        <div>{ i18n._( t`Your total pool tokens` ) }:</div>
+                        <div className="font-semibold">{ formatNumber( balance?.toExact(), false, false, 6 ) }</div>
                     </RowBetween>
+
+                    {
+                        liquidityTokensValue && liquidityTokensValue.map( ( token, index ) =>
+                            <RowBetween key={ index }>
+                                <div className='flex items-center space-x-4'>
+                                    { i18n._( t`Pooled ${token?.currency?.symbol}` ) }
+                                    <div className='mx-1'>
+                                        <QuestionHelper
+                                            text={ i18n._( t`Estimated from liquidity token` ) }
+                                        />
+                                    </div>:
+                                </div>
+                                <div className="font-semibold">{ formatNumber( token?.toExact(), false, false, 6 ) }</div>
+                            </RowBetween>
+                        )
+                    }
 
                     <RowBetween>
                         <div>{ i18n._( t`Your pool share` ) }:</div>
-                        <div className="font-semibold">{ poolTokenPercentage ? formatPercent( poolTokenPercentage ) : '-' }</div>
+                        <div className="font-semibold">{ formatNumberPercentage( balance?.toExact(), totalLpSupply?.toExact() ) }</div>
                     </RowBetween>
                 </div>
             ) }
@@ -49,4 +78,4 @@ const StablePoolInfo = ( {
     )
 }
 
-export default StablePoolInfo
+export default StablePoolPosition;
