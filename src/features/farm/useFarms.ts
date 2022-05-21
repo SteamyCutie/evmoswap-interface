b658f86dcad237b188ada772284d2f46f9934930
Madandola Hammed <aramicrown@gmail.com>
2022-05-21 09:08:15 +0100

    fix: farm load on chain switch

--- src/features/farm/useFarms.ts
+++ src/features/farm/useFarms.ts
@@ -6,48 +6,48 @@ import { useActiveWeb3React } from '../../services/web3'
 
 // Todo: Rewrite in terms of web3 as opposed to subgraph
 const useFarms = () => {
-  const { chainId } = useActiveWeb3React()
+    const { chainId } = useActiveWeb3React()
 
-  const [farms, setFarms] = useState<any | undefined>()
-  const dashboardContract = useDashboardContract()
+    const [ farms, setFarms ] = useState<any | undefined>()
+    const dashboardContract = useDashboardContract()
 
-  const fetchAllFarms = useCallback(async () => {
-    // Reset pools list
-    const farmingPools = Object.keys(FARMS[chainId]).map((key) => {
-      return { ...FARMS[chainId][key], lpToken: key }
-    })
+    const fetchAllFarms = useCallback( async () => {
+        // Reset pools list
+        const farmingPools = Object.keys( FARMS[ chainId ] ).map( ( key ) => {
+            return { ...FARMS[ chainId ][ key ], lpToken: key }
+        } )
 
-    // Array Pids
-    const poolPids = farmingPools.map((pool: any) => {
-      return pool.pid
-    })
-    const { tokenPrice, totalTvlInUSD, allocPoint, apy, tvl, tvlInUSD } = await dashboardContract?.infoOfPools(poolPids)
-    const farms = farmingPools.map((pool: any) => {
-      return {
-        ...pool,
-        chef: 0,
-        type: 'ELP',
-        tokenPrice: tokenPrice / 1e18,
-        totalTvlInUSD: totalTvlInUSD / 1e18,
-        flpBalance: tvl[pool.id] / 1e18,
-        tvl: tvlInUSD[pool.id] / 1e18,
-        apr: apy[pool.id] / 1e16,
-        lpPrice: tvlInUSD[pool.id] / tvl[pool.id],
-        multiplier: Number(allocPoint[pool.id]),
-      }
-    })
+        // Array Pids
+        const poolPids = farmingPools.map( ( pool: any ) => {
+            return pool.pid
+        } )
+        const { tokenPrice, totalTvlInUSD, allocPoint, apy, tvl, tvlInUSD } = await dashboardContract?.infoOfPools( poolPids )
+        const farms = farmingPools.map( ( pool: any ) => {
+            return {
+                ...pool,
+                chef: 0,
+                type: 'ELP',
+                tokenPrice: tokenPrice / 1e18,
+                totalTvlInUSD: totalTvlInUSD / 1e18,
+                flpBalance: tvl[ pool.id ] / 1e18,
+                tvl: tvlInUSD[ pool.id ] / 1e18,
+                apr: apy[ pool.id ] / 1e16,
+                lpPrice: tvlInUSD[ pool.id ] / tvl[ pool.id ],
+                multiplier: Number( allocPoint[ pool.id ] ),
+            }
+        } )
 
-    // console.log('farms:', farms)
-    const sorted = _.orderBy(farms, ['multiplier'], ['desc'])
+        // console.log('farms:', farms)
+        const sorted = _.orderBy( farms, [ 'multiplier' ], [ 'desc' ] )
 
-    setFarms({ farms: sorted, userFarms: [], stakeFarms: [] })
-  }, [])
+        setFarms( { farms: sorted, userFarms: [], stakeFarms: [] } )
+    }, [ chainId, dashboardContract ] )
 
-  useEffect(() => {
-    fetchAllFarms()
-  }, [fetchAllFarms])
+    useEffect( () => {
+        fetchAllFarms()
+    }, [ fetchAllFarms ] )
 
-  return farms
+    return farms
 }
 
 export default useFarms
