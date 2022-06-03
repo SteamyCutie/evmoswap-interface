3d0a8239f124b0667b20dd52516a5dc318a724a4
Madandola Hammed <aramicrown@gmail.com>
2022-06-03 13:17:26 +0100

    feat: add className props

--- src/components/QuestionHelper/index.tsx
+++ src/components/QuestionHelper/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useCallback, useState } from 'react'
 import { QuestionMarkCircleIcon as SolidQuestionMarkCircleIcon } from '@heroicons/react/solid'
 import Tooltip from '../Tooltip'
 
-const QuestionHelper: FC<{ text?: any }> = ( { children, text } ) => {
+const QuestionHelper: FC<{ text?: any, className?: string }> = ( { children, text, className } ) => {
     const [ show, setShow ] = useState<boolean>( false )
 
     const open = useCallback( () => setShow( true ), [ setShow ] )
@@ -12,7 +12,7 @@ const QuestionHelper: FC<{ text?: any }> = ( { children, text } ) => {
         return (
             <Tooltip text={ text } show={ show }>
                 <div
-                    className="flex items-center justify-center outline-none"
+                    className={ `flex items-center justify-center outline-none ${className}` }
                     onClick={ open }
                     onMouseEnter={ open }
                     onMouseLeave={ close }
