87d93fa74ff146b9016935eba795d0993d06cb57
Madandola Hammed <aramicrown@gmail.com>
2022-06-18 08:04:28 +0100

    fix: update star background theme

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -3,18 +3,17 @@ import Header from '../../components/Header'
 import Main from '../../components/Main'
 import Popups from '../../components/Popups'
 
-const Layout = ({ children }) => {
-  return (
-    <div
-      className="z-0 flex flex-col items-center w-full min-h-screen bg-light-primary dark:bg-dark-primary transition-all"
-      style={{ backgroundImage: 'url(/star.svg)' }}
-    >
-      <Header />
-      <Main>{children}</Main>
-      <Popups />
-      <Footer />
-    </div>
-  )
+const Layout = ( { children } ) => {
+    return (
+        <div
+            className="main-wrapper z-0 flex flex-col items-center w-full min-h-screen bg-light-primary dark:bg-dark-primary transition-all"
+        >
+            <Header />
+            <Main>{ children }</Main>
+            <Popups />
+            <Footer />
+        </div>
+    )
 }
 
 export default Layout
