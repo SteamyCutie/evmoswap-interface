ba92273b3dce14c68e90592c68374944eca94d98
Madandola Hammed <aramicrown@gmail.com>
2022-07-30 22:12:05 +0100

    fix: apply farm reward rate constant
    clean up unsued code

--- src/features/farm/hooks.ts
+++ src/features/farm/hooks.ts
@@ -13,9 +13,10 @@ import { NEVER_RELOAD, useMultipleContractSingleData, useSingleCallResult } from
 import { useCurrency, useToken } from 'app/hooks/Tokens'
 import { Token } from '@evmoswap/core-sdk'
 import ERC20_INTERFACE from 'app/constants/abis/erc20'
-import { PairInfo, TokenInfo } from 'app/constants/farms'
+import { FARMS_REWARD_RATE_PER_SEC, PairInfo, TokenInfo } from 'app/constants/farms'
 import { formatBalance, formatNumber } from 'app/functions'
 import { formatBigNumber, formatBigNumberToFixed } from 'app/functions/formatBalance'
+import { useWeb3React } from '@web3-react/core'
 
 // @ts-ignore TYPE NEEDS FIXING
 export function useUserInfo(farm, token) {
@@ -79,69 +80,13 @@ export function useFarmPendingRewardsAmount(farm) {
 }
 
 export function useFarmRewardsApr(farm?: PairInfo, rewards?: Token[]) {
-  const token0APR = useFarmRewardApr(rewards?.[0], farm?.pid)
-  const token1APR = useFarmRewardApr(rewards?.[1], farm?.pid)
-  return ([token0APR, token1APR] || [0, 0]).slice(0, rewards?.length)
+  const reward0APR = useFarmRewardApr(rewards?.[0], farm?.pid)
+  const reward1APR = useFarmRewardApr(rewards?.[1], farm?.pid)
+  return ([reward0APR, reward1APR] || [0, 0]).slice(0, rewards?.length)
 }
 
-export async function getFarmRewardApr(masterchef, dashboardContract, token: Token, pid?: any) {
-  const data = await Promise.all([
-    masterchef.TOKENLESS_PRODUCTION(),
-    masterchef.emoPerSecond(),
-    dashboardContract.rewardPerYearOfPool(pid),
-    masterchef.poolInfo(pid),
-  ])
-  data.slice(0, data.length - 1).forEach((val, index) => {
-    console.log(val)
-
-    data[index] = BigNumber.from(val.toString())
-  })
-  console.log(data)
-
-  const [TOKENLESS_PRODUCTION, emoPerSec, emoRewardPerYearOfPool, poolInfo] = data
-
-  const workingSupply = poolInfo?.workingSupply?.toString()
-  const lpAddress = poolInfo?.lpToken
-
-  const rewardAddress = token?.address //useSingleCallResult(dashboardContract, 'reward')?.result?.[0]
-  const decimals = token?.decimals
-  const tokenPerSec = String(3.25).toBigNumber(decimals)
-  //const apy = useSingleCallResult(dashboardContract, 'apyOfPool', [pid])?.result?.[0]
-  //const tokenInfo = useTokenInfo(useTokenContract(token.id))
-  console.log(lpAddress)
-  const data2 = await Promise.all([
-    dashboardContract.valueOfAsset(rewardAddress, '1'.toBigNumber(decimals)),
-    dashboardContract.valueOfAsset(lpAddress, workingSupply),
-  ])
-  console.log(data2)
-  data2.forEach((val, index) => (data2[index] = BigNumber.from(val[0].toString())))
-
-  const [rewardPriceInETH, valueInETH] = data2
-
-  const rewardPerYearOfPool =
-    emoRewardPerYearOfPool && emoPerSec ? emoRewardPerYearOfPool.div(emoPerSec).mul(tokenPerSec) : '0'
-
-  //console.log({ rewardPriceInETH, rewardPerYearOfPool, TOKENLESS_PRODUCTION, valueInETH }, poolInfo)
-
-  if (!rewardPriceInETH || !valueInETH || !TOKENLESS_PRODUCTION || !rewardPerYearOfPool) {
-    console.warn('Missing data for', pid, { rewardPriceInETH, rewardPerYearOfPool, TOKENLESS_PRODUCTION, valueInETH })
-    return '0'
-  }
-
-  //return String(apy / 1e16)
-  return formatBigNumber(TOKENLESS_PRODUCTION.mul(rewardPriceInETH).mul(rewardPerYearOfPool).div(valueInETH))
-
-  /*
-  return (
-    (Number(formatBigNumber(TOKENLESS_PRODUCTION)) *
-      Number(formatBigNumber(rewardPriceInETH)) *
-      Number(rewardPerYearOfPool)) /
-    Number(formatBigNumber(valueInETH)) /
-    100
-  )*/
-}
-
-export function useFarmRewardApr(token: Token, pid?: any, tokenPerSec: number = 0) {
+export function useFarmRewardApr(token: Token, pid?: any) {
+  const { chainId } = useWeb3React()
   const dashboardContract = useDashboardContract()
   const masterchef = useMasterChefContract()
 
@@ -152,11 +97,10 @@ export function useFarmRewardApr(token: Token, pid?: any, tokenPerSec: number = 
   const workingSupply = poolInfo?.workingSupply
   const lpAddress = poolInfo?.lpToken
 
-  const rewardAddress = token?.address //useSingleCallResult(dashboardContract, 'reward')?.result?.[0]
+  const rewardAddress = token?.address
   const decimals = token?.decimals
+  const tokenPerSec = FARMS_REWARD_RATE_PER_SEC[chainId][rewardAddress]
   const rate = tokenPerSec > 0 ? String(tokenPerSec).toBigNumber(decimals) : emoPerSec
-  //const apy = useSingleCallResult(dashboardContract, 'apyOfPool', [pid])?.result?.[0]
-  //const tokenInfo = useTokenInfo(useTokenContract(token.id))
 
   const rewardPriceInETH = useSingleCallResult(
     dashboardContract,
@@ -169,10 +113,7 @@ export function useFarmRewardApr(token: Token, pid?: any, tokenPerSec: number = 
 
   const valueInETH = useSingleCallResult(dashboardContract, 'valueOfAsset', [lpAddress, workingSupply])?.result?.[0]
 
-  //console.log({ rewardPriceInETH, rewardPerYearOfPool, TOKENLESS_PRODUCTION, valueInETH }, poolInfo)
-
   if (!rewardPriceInETH || !valueInETH || !TOKENLESS_PRODUCTION || !rewardPerYearOfPool) {
-    console.warn('Missing data for', pid, { rewardPriceInETH, rewardPerYearOfPool, TOKENLESS_PRODUCTION, valueInETH })
     return '0'
   }
 
